3a7a5b8ecfcb8adf53d222d2888adecdfb51a010
Rogienald Agol <rogienaldremu@gmail.com>
2022-05-13 22:56:26 +0800

    updated

--- pages/components/PDFPrint.js
+++ pages/components/PDFPrint.js
@@ -18,35 +18,41 @@ export default function PDFPrint() {
 		 
 
 		pdf.setFontSize(30)
-		pdf.setFont(undefined, 'bold').text("Romar Desabille", startY, 20)
+		pdf.setFont(undefined, 'bold').text("CourseMe", startY, 20)
 			.setFont(undefined, 'normal')
 
-		pdf.setFontSize(10)
-		pdf.text(`Date Taken ${dateTaken}`, startY, 25);
+		pdf.setFontSize(12)
+		pdf.setFont(undefined, 'bold').text(`Name: `, startY, 27);
+		pdf.setFont(undefined, 'normal').text(`Student name`, startY+15, 27);
+
+		pdf.setFont(undefined, 'bold').text(`Date: `, startY, 34);
+		pdf.setFont(undefined, 'normal').text(`${dateTaken}`, startY+13, 34);
+
+		var spaceInfo = 48;
+		
 
 		pdf.setFontSize(18)
-		pdf.setFont(undefined, 'bold').text(`Student Information`, startY, 40);
+		pdf.setFont(undefined, 'bold').text(`Student Information`, startY, spaceInfo);
 
 		pdf.setFontSize(12)
-		pdf.setFont(undefined, 'normal').text(`Strand: `, startY, 47);
-		pdf.setFont(undefined, 'normal').text(`ICT`, startY+20, 47);
+		pdf.setFont(undefined, 'bold').text(`Strand: `, startY, spaceInfo+8);
+		pdf.setFont(undefined, 'normal').text(`ICT`, startY+17, spaceInfo+8);
 
-		pdf.setFont(undefined, 'normal').text(`Age: `, startY, 54);
-		pdf.setFont(undefined, 'normal').text(`17`, startY+20, 54);
+		pdf.setFont(undefined, 'bold').text(`Age: `, startY, spaceInfo+15);
+		pdf.setFont(undefined, 'normal').text(`17`, startY+12, spaceInfo+15);
 
-		pdf.setFont(undefined, 'normal').text(`School: `, startY, 61);
-		pdf.setFont(undefined, 'normal').text(`STI`, startY+20, 61);
+		pdf.setFont(undefined, 'bold').text(`School from: `, startY, spaceInfo+22);
+		pdf.setFont(undefined, 'normal').text(`STI`, startY+28, spaceInfo+22);
 
-		pdf.setFont(undefined, 'normal').text(`Gender: `, startY, 68);
-		pdf.setFont(undefined, 'normal').text(`Male`, startY+20, 68);
+		pdf.setFont(undefined, 'bold').text(`Gender: `, startY, spaceInfo+29);
+		pdf.setFont(undefined, 'normal').text(`Male`, startY+19, spaceInfo+29);
 
-
-		var spaceExam = 10*4; //3 lines for subject + exam result title = 4
+		var spaceExam = spaceInfo+45; 
 
 		pdf.setFontSize(18)
-		pdf.setFont(undefined, 'bold').text(`Exam Result`, startY, 40+spaceExam);
+		pdf.setFont(undefined, 'bold').text(`Exam Result`, startY, spaceExam);
 
-		pdf.setFontSize(12)
+		/* pdf.setFontSize(12)
 		pdf.setFont(undefined, 'normal').text(`English: `, startY, 47+spaceExam);
 		pdf.setFont(undefined, 'normal').text(`15`, startY+20, 47+spaceExam);
 
@@ -54,10 +60,33 @@ export default function PDFPrint() {
 		pdf.setFont(undefined, 'normal').text(`17`, startY+20, 54+spaceExam);
 
 		pdf.setFont(undefined, 'normal').text(`Math: `, startY, 61+spaceExam);
-		pdf.setFont(undefined, 'normal').text(`17`, startY+20, 61+spaceExam);
+		pdf.setFont(undefined, 'normal').text(`17`, startY+20, 61+spaceExam); */
+
+		autoTable(pdf, { html: '#my-table' })
+
+		// Or use javascript directly:
+		autoTable(pdf, {
+			theme: 'grid',
+			startY: spaceExam+5,
+			styles: {
+				fontStyle: 'bold',
+				textColor: 20,
+				lineColor: 20,
+			},
+			head: [['Subject', 'Score']],
+			body: [
+				['English', '35/35'],
+				['Math', '35/35'],
+				['Science', '35/35'],
+			],
+			bodyStyles: { fontStyle: 'normal' },
+			columnStyles: { 
+				0: { cellWidth: 60 },
+			 },
+		})
 
 
-		var spaceRecommendCourse = 68+spaceExam+5;
+		var spaceRecommendCourse = spaceExam + (4*10) + 10;
 
 		pdf.setFontSize(18);
 		pdf.setFont(undefined, 'bold').text(`Course Recommended`, startY, spaceRecommendCourse);
@@ -84,7 +113,10 @@ export default function PDFPrint() {
 			columnStyles: { 1: { halign: 'left' } },
 		})
 
-		var spaceRegression = spaceRecommendCourse + (4*10) + 6; //3 rank line + table header = 4
+		var checkCountCharCourse = false;
+
+
+		var spaceRegression = spaceRecommendCourse + (4*10) + 10; //3 rank line + table header = 4
 
 		pdf.setFontSize(18);
 		pdf.setFont(undefined, 'bold').text(`Regression Model`, startY, spaceRegression);
